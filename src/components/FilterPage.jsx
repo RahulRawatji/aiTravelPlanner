6dc1ee08c4572c1951a1af569624757305174b97
rahulrawat <rahulrawatwork@gmail.com>
2023-04-07 00:11:16 +0530

    feat: :art: responsive fix

--- src/components/FilterPage.jsx
+++ src/components/FilterPage.jsx
@@ -4,33 +4,57 @@ import { useState } from "react";
 
 const FilterPage = () => {
   const [showMoreFilter, setShowMoreFilter] = useState(false);
+  const [values, setValues] = useState({});
   const personaliseHandler = () => {
-    setShowMoreFilter(true)
+    setShowMoreFilter(true);
   };
 
-  const renderMoreFilter = () =>{
-    return (  <>{mainFilters.map(filter =>{
-      const {title, icon, inputType, inputPlaceholder } =filter;
-        return (<div key={title} className="flex items-center bg-[#EDF4F7] px-3 py-2 gap-4 w-1/4 rounded-md">
-          {icon}
-        <div className="ml-1">
-          <span className="text-sm font-semibold">{title}</span>
-          <input
-            type={inputType}
-            className="border-none w-full bg-[#EDF4F7]"
-            placeholder={inputPlaceholder}
-          />
+  const renderMoreFilter = () => {
+    return (
+      <>
+        <div className="bg-white px-6 py-5 rounded-tr-3xl w-4/5 flex  gap-5">
+          {personalisedFilter.map((filter) => {
+            const { title, icon, inputType, inputPlaceholder } = filter;
+            return (
+              <div
+                key={title}
+                className="flex items-center bg-[#EDF4F7] px-3 py-2 gap-4 w-1/3 rounded-md"
+              >
+                {icon}
+                <div className="ml-1">
+                  <span className="text-sm font-semibold">{title}</span>
+                  <input
+                    type={inputType}
+                    className="border-none w-full bg-[#EDF4F7]"
+                    placeholder={inputPlaceholder}
+                  />
+                </div>
+              </div>
+            );
+          })}
         </div>
-      </div>)
-    })}</>)
-  }
+      </>
+    );
+  };
 
   const personalisedFilter = [
     {
-      title: "Destination",
+      title: "Activity",
       icon: <GrLocation size={50} />,
-      inputType:'select',
-      inputPlaceholder:"Enter Destination"
+      inputType: "select",
+      inputPlaceholder: "Water Sport",
+    },
+    {
+      title: "Budget",
+      icon: <GrLocation size={50} />,
+      inputType: "select",
+      inputPlaceholder: "Water Sport",
+    },
+    {
+      title: "Cusines",
+      icon: <GrLocation size={50} />,
+      inputType: "select",
+      inputPlaceholder: "Water Sport",
     },
   ];
 
@@ -38,45 +62,47 @@ const FilterPage = () => {
     {
       title: "Destination",
       icon: <GrLocation size={50} />,
-      inputType:'text',
-      inputPlaceholder:"Enter Destination"
+      inputType: "text",
+      inputPlaceholder: "Enter Destination",
     },
     {
-      title:"Travel Date",
-      icon:<AiOutlineCalendar size={50} />,
-      inputType:'date',
-      inputPlaceholder:""
-
+      title: "Travel Date",
+      icon: <AiOutlineCalendar size={50} />,
+      inputType: "date",
+      inputPlaceholder: "",
     },
     {
       title: "Days",
-      icon:<AiOutlineUser size={50} />,
-      inputType:'number',
-      inputPlaceholder:"Enter Days"
-    }
+      icon: <AiOutlineUser size={50} />,
+      inputType: "number",
+      inputPlaceholder: "Enter Days",
+    },
   ];
-
 
   return (
     <div className="flex items-center bg-orange-300 h-screen">
-      <div className="mx-auto w-3/5">
-        <div className="bg-white px-6 py-5 rounded-tr-3xl flex gap-5">
-
-          {mainFilters.map(filter =>{
-            const {title, icon, inputType, inputPlaceholder } =filter;
-              return (<div key={title} className="flex items-center bg-[#EDF4F7] px-3 py-2 gap-4 w-1/4 rounded-md">
+      <div className="mx-auto w-9/12 max-w-4xl gap-1 flex flex-col">
+        <div className="bg-white px-6 py-5 rounded-tr-3xl flex justify-evenly gap-2 flex-wrap sm:grid sm:grid-cols-2 lg:flex">
+          {mainFilters.map((filter) => {
+            const { title, icon, inputType, inputPlaceholder } = filter;
+            return (
+              <div
+                key={title}
+                className="flex items-center bg-[#EDF4F7] lg:w-1/4 px-3 py-2 gap-2 rounded-md"
+              >
                 {icon}
-              <div className="ml-1">
-                <span className="text-sm font-semibold">{title}</span>
-                <input
-                  type={inputType}
-                  className="border-none w-full bg-[#EDF4F7]"
-                  placeholder={inputPlaceholder}
-                />
+                <div className="ml-1">
+                  <span className="text-sm font-semibold">{title}</span>
+                  <input
+                    type={inputType}
+                    className="border-none w-full bg-[#EDF4F7] basis-4"
+                    placeholder={inputPlaceholder}
+                  />
+                </div>
               </div>
-            </div>)
+            );
           })}
-          
+
           <button
             className="p-5 font-semibold rounded-full bg-[#1B485A] text-white"
             variant="contained"
@@ -84,14 +110,20 @@ const FilterPage = () => {
             Build Itinerary
           </button>
         </div>
-        <div className="flex justify-end">
-         { showMoreFilter? renderMoreFilter() : <a
-            className="mt-3 mr-4 text-center font-semibold text-lg p-2 rounded-lg cursor-pointer hover:bg-white"
-            onClick={personaliseHandler}
-          >
-            💡Personalise
-          </a>}
-        </div>
+        <>
+          {showMoreFilter ? (
+            renderMoreFilter()
+          ) : (
+            <div className="flex justify-end">
+            <a
+              className="mt-3 mr-4 text-center font-semibold text-lg p-2 rounded-lg cursor-pointer hover:bg-white"
+              onClick={personaliseHandler}
+            >
+              💡Personalise
+            </a>
+            </div>
+          )}
+        </>
       </div>
     </div>
   );
