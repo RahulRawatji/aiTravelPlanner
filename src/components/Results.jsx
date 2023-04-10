b3318be1261796d4430a2ddc82efb4e1cd987edb
rahulrawat <rahulrawatwork@gmail.com>
2023-04-10 16:20:17 +0530

    feat: :lipstick: added some styling to results page

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -9,12 +9,12 @@ const Results = () => {
   
   let formatted_data  = data.split('\n\n');
   return (
-    <div>
+    <div className='bg-cyan-200 pb-10'>
       <h3 className='text-3xl text-center py-10'> ⭐️ Travel Planner ⭐️</h3>
-      <div className='w-3/5 mx-auto font-poppins leading-loose'>
+      <div className='w-3/5 mx-auto bg-gray-300 font-poppins border border-black p-5 rounded-md '>
         {
           formatted_data.map(f_data =>{
-            return <div className='my-4'>
+            return <div className='my-4 leading-loose'>
                     <ReactMarkdown>{f_data}</ReactMarkdown>
                   </div>
           })
